726d6cdbd27cfa157b7fbe74431ef46e63634806
Silver2005 <mailto:156700600+Silver2005@users.noreply.github.com>
2026-03-08 00:38:30 +0000

    Version Finale SILVER-FIN : UI, Branding et Fixes

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://TON_PROJET.supabase.co'; // À REMPLACER
-const supabaseAnonKey = 'TA_CLE_ANON_ICI';          // À REMPLACER
+// Configuration de ton projet SILVER-FIN Cloud
+const supabaseUrl = 'https://tfvjzuvnrslbcluflshs.supabase.co';
+const supabaseAnonKey = 'sb_publishable_Jx82WIlIHqCcXuvY3iH4PA_Hp4SCBVt';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
